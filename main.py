299c80576f7b674b18c93e3161662f6f9e6e3951
s4d <147156965+joaopedro124@users.noreply.github.com>
2025-02-23 18:16:14 -0300

    wip

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+import json, os
+
+app = Flask(__name__)
+CORS(app)
+database_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "database.json")
+
+# Function for loading Data
+def load_data() -> dict:
+    with open(database_file, "r", encoding="utf-8") as database:
+        return json.load(database)
+
+# Function for saving Data
+def save_data(data: dict) -> None:
+    with open(database_file, "w", encoding="utf-8") as database:
+        json.dump(data, database, indent=4)
+
+# Flask route for getting Aulas
+@app.route("/aulas", methods=["GET"])
+def get_aulas():
+    data = load_data()
+    return jsonify(data["aulas"])
+
+# Flask route for adding Aulas
+@app.route("/aulas", methods=["POST"])
+def add_aulas():
+    data = load_data()
+    newAula = request.json
+
+    # New ID for Aula
+    newAula["id"] = len(data["aulas"]) + 1 # Aulas amount + 1
+    data["aulas"].append(newAula)
+
+    # Save and return new data
+    save_data(data)
+    return jsonify({
+        "Mensagem do Servidor": "Aula Adicionada com Sucesso!"
+    }), 200
+
+# Flask route for getting Alunos
+@app.route("/alunos", methods=["GET"])
+def get_alunos():
+    data = load_data()
+    return jsonify(data["alunos"])
+
+# Flask route for adding Alunos
+@app.route("/alunos", methods=["POST"])
+def add_alunos():
+    data = load_data()
+    newAluno = request.json
+
+    # New ID for Aluno
+    newAluno["id"] = len(data["alunos"]) + 1 # Alunos amount + 1
+    data["alunos"].append(newAluno)
+
+    # Save and return new data
+    save_data(data)
+    return jsonify({
+        "Mensagem do Servidor": "Aluno cadastrado com Sucesso!"
+    }), 200
+
+@app.route("/")
+def home():
+    return "<h1>Error: 404</h1>"
+
+app.run(host="0.0.0.0", port=5000)
